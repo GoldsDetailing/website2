3f99eda8a73fde40f7ca3bfceb8831592b7f4526
Trent Green <trgreen2002@gmail.com>
2022-03-24 19:39:56 -0400

    added favicon

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
+var favicon = require('serve-favicon');
+
 
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+
+
 
 const port = process.env.PORT || 3000;
 
@@ -14,5 +20,6 @@ app.set('view engine', 'ejs');
 app.use('/', require('./routes/index'));
 app.use('/services', require('./routes/services'));
 app.use('/gallery', require('./routes/gallery'));
+
 
 app.listen(port, console.log(`server started on ${port}`));
